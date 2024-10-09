5aca2ce4309a6d1ea7dd8e93db35d03e76600e84
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-10-09 23:30:28 +0200

    Another Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Heading from "./Heading";
 
-function Header() {
-  return <h1>Hello World</h1>;
-}
-
-function App() {
-  return <Header />; 
+function App(){
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
 }
 
 export default App;
