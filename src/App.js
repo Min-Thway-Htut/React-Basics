4d9fdce3d2cc90030f3f3f0e3b8c0cdbe4f5eabd
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-10-10 21:07:58 +0200

    Another update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Heading from "./Heading";
+import Bag from "./Bag";
+import Apples from "./Apples";
+import Pears from "./Pears";
 
 function App(){
   return (
     <div className="App">
       <Heading firstName="Bob" />
       <Heading firstName="Any name other than Bob" />
+      <Bag>
+        <Apples color="yellow" number="5" />
+        <Pears friend="Peter" />
+      </Bag>
     </div>
   );
 }
