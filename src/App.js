6d01e31056b33c1b0f5830e7fba6823dad7dc217
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-10-16 15:56:11 +0200

    ToggleMode

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Bag from "./Bag";
 import Apples from "./Apples";
 import Pears from "./Pears";
 import Card from "./Card";
+import Btn from "./Btn";
+import ModeToggler from "./ModeToggler";
+
 
 function App(){
   return (
     <div className="App">
+      <Btn />
       <h1>Task: Add three Card elements</h1>
       <Card h2="First card's h2" h3="First card's h3" />
       <Card h2="Second card's h2" h3="Second card's h3" />
@@ -18,6 +22,8 @@ function App(){
         <Apples color="yellow" number="5" />
         <Pears friend="Peter" />
       </Bag>
+
+      <ModeToggler />
     </div>
   );
 }
