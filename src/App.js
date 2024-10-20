26912b73cc24f1fc849460aab1917071dd32e87e
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-10-20 21:35:02 +0200

    Adding Audio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
-import {Routes, Route, Link} from "react-router-dom";
 import "./App.css";
-import Contact from "./Contact";
-import Homepage from "./Homepage";
-import AboutLittleLemon from "./AboutLittleLemon";
-import javaScript from "./assets/javaScript.png";
+import ReactPlayer from "react-player/youtube";
 
 
 function App(){
+
+  const bird1 = new Audio("https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3");
+
+  const bird2 = new Audio("https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3");
+  
+  function toggle1(){
+    if(bird1.paused){
+      bird1.play();
+    }else{
+      bird1.pause();
+    }
+  };
+
+  function toggle2(){
+    if(bird2.paused){
+      bird2.play();
+    }else{
+      bird2.pause();
+    }
+  }
+
   return(
     <div>
-      <h1>Task: Add an image below</h1>
-      <img src={javaScript} alt="javaScript Logo" />
-      <nav>
-        <Link to="/" className="nav-item">Homepage</Link>
-        <Link to="/about" className="nav-item">About Little Lemon</Link>
-        <Link to="/contact" className="nav-item"></Link> 
-      </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/about" element={<AboutLittleLemon />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
+      <div>
+        <MyVideo />
+        <button onClick={toggle1}>Click here</button>
+        <button onClick={toggle2}>Click here</button>
+      </div>
     </div>
+  )
+}
+
+const MyVideo = () => {
+  return(
+    <ReactPlayer url="https://youtu.be/6IBDljy4fos?si=di4ZtCviRG2UyRNN" playing={true} volume={0.5}/>
   )
 }
 
